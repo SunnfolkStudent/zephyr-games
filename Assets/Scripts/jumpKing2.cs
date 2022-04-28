4c06224e655075b2a02549aa7aaed86f7df657f0
PowerBiscuit <sballe667@gmail.com>
2022-04-28 11:27:16 +0200

    Jump ferdig
    Er jump ferdig? Finn ut på neste episode av dragon ball z

--- Assets/Scripts/jumpKing2.cs
+++ Assets/Scripts/jumpKing2.cs
@@ -15,25 +15,27 @@ public class jumpKing2 : MonoBehaviour
     public float jumpValueCounter = 10f;
     public float maxJumpValue = 10;
 
-
+    private Dash _dash;
     private Input input;
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         input = GetComponent<Input>();
+        _dash = GetComponent<Dash>();
     }
 
     void Update()
     {
+        isGrounded = Physics2D.OverlapBox(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 1.1f),
+            new Vector2(0.9f, 0.4f), 0f, groundMask);
+        
+        if (_dash._isDashing) return;
 
         if (jumpValue == 0.0f && isGrounded)
         {
             rb.velocity = new Vector2(input.MoveInput.x * walkSpeed, rb.velocity.y);
         }
-
-        isGrounded = Physics2D.OverlapBox(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 1.1f),
-        new Vector2(0.9f, 0.4f), 0f, groundMask);
-
+        
         if(jumpValue > 0)
         {
             rb.sharedMaterial = bounceMat;
