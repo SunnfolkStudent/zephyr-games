998c9080daca04a6914b24b05a4f7d12e561330b
PowerBiscuit <sballe667@gmail.com>
2022-05-09 10:03:46 +0200

    Final changes... Kanskje??

--- Assets/EndScene.cs
+++ Assets/EndScene.cs
@@ -10,29 +10,22 @@ public class EndScene : MonoBehaviour
     
     private float Timer = 0;
     private Animator _animator;
+    private SpriteRenderer _renderer;
     
     private void Start()
     {
-        
+        _renderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
     }
-
-    private void Update()
-    {
-        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("n"))
-        {
-            if (Timer < Time.time)
-            {
-                SceneManager.LoadScene("WinMenu");
-            }
-        }
-    }
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            Timer = Time.time + _animator.GetCurrentAnimatorStateInfo(0).length + 2f;
+           // Timer = Time.time + _animator.GetCurrentAnimatorStateInfo(0).length + 2f;
+           _renderer.sortingOrder = 100;
             _animator.Play("n");
+            other.gameObject.SetActive(false);
         }
     }
     public void LoadEnd()
